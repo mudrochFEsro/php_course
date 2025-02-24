c97b58e54064ee8d33263630892720b417719d9c
Juraj Mudroch <mudroch.juraj@protonmail.com>
2025-02-24 12:13:56 +0100

    push

--- http-and-web/session-cookies.php
+++ http-and-web/session-cookies.php
@@ -1,9 +1,16 @@
 <?php
+if($_SERVER['REQUEST_METHOD'] === '/favicon.ico') {
+    exit;
+}
 session_start();
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    setcookie('user', $_POST['name'], time() + (3600), "/");
+}
 $hasCookie = isset($_COOKIE['user']);
 if (!$hasCookie) {
     $welcomeMessage = "Welcome back user!";
 } else {
+
     $welcomeMessage = "Welcome back " . htmlspecialchars($_COOKIE['user']);
 }
 if (!isset($_SESSION['visits'])) {
@@ -18,7 +25,7 @@ $visitMessage = "This is your " . $_SESSION['visits'] . " visit.";
 <html lang="">
 <body>
 <?php if (!$hasCookie): ?>
-    <form method="POST">
+    <form method="POST" >
         <label for="name">Your name:</label>
         <input id="name" type="text" name="name"/>
         <button>Track</button>
